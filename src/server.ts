677cc72c96267ff8a1a64bcef0d90fdeaec93e1b
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-11-01 19:09:48 -0300

    feat:validation with zod and generate code with short unique id

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
-
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
   log: ['query']
@@ -21,6 +22,39 @@ async function bootstrap() {
 
     return { count }
   })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string({
+        required_error: "Title is required",
+        invalid_type_error: "Title must be a string",
+      }),
+    })
+   
+      try {
+        const { title } = createPoolBody.parse(request.body)
+
+        const generate = new ShortUniqueId({ length:6 })
+        const code = String(generate()).toUpperCase()
+        await prisma.pool.create({
+          data: {
+            title,
+            code 
+          }
+        })
+        return reply.status(201).send({code})
+      } catch (error) {
+        const errorJson = JSON.stringify(error)
+        if (error instanceof z.ZodError){
+          return reply.status(400).send(errorJson)
+        } 
+        else {
+            return reply.status(500).send(errorJson)
+        }
+      }
+  })
+
+  
 
   await fastify.listen({ port: 3333, /*host: '0.0.0.0' */})
 }
